e32ab8b2fb31707784db84df92fa360ecf153ac9
abhinavp403 <abhinavp403@gmail.com>
2023-11-25 19:04:20 -0500

    Race tracker activity with coroutines

---
+++ app/src/main/java/dev/abhinav/composeapps/RaceTrackerActivity.kt
@@ -0,0 +1,27 @@
+package dev.abhinav.composeapps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dev.abhinav.composeapps.ui.theme.ComposeAppsTheme
+
+class RaceTrackerActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    RaceTrackerApp()
+                }
+            }
+        }
+    }
+}
